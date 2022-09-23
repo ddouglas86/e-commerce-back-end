120cca4b460e69c22631c983ffafdb57f44182bd
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-09-22 21:32:42 -0500

    added get route for single tag by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -18,7 +18,21 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+    // be sure to include its associated Product data
+      include: [{ model: Product, through: ProductTag }],
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'Invalid Category' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', async (req, res) => {
